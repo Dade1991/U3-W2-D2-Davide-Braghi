27665509000e09d6cf297b22e644da4c9eb9a24b
Dade1991 <dade91@msn.com>
2025-07-29 18:07:19 +0200

    some changes & added solution

--- epibooks-4-main/src/components/AddComment.jsx
+++ epibooks-4-main/src/components/AddComment.jsx
@@ -1,101 +1,110 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button, Form } from "react-bootstrap"
 
 const AddComment = (props) => {
-  const [comment, setComment] = useState("")
-  const [rate, setRate] = useState(1)
-  const [elementId, setElementId] = useState(props.asin)
+  const [comment, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: props.asin,
+  })
+}
+class AddComment extends Component {
+  state = {}
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        comment: {
+          ...this.state.comment,
+          elementId: this.props.asin,
+        },
+      })
+    }
+  }
 
-useEffect (() => {
-        const sendComment = async (e) => {
-      e.preventDefault(){
-        setComment({
+  sendComment = async (e) => {
+    e.preventDefault()
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments",
+        {
+          method: "POST",
+          body: JSON.stringify(this.state.comment),
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODcwYmIzMjc4Y2RkZjAwMTU1ZDY3OWYiLCJpYXQiOjE3NTM3OTIzMjMsImV4cCI6MTc1NTAwMTkyM30.iucPMUxGD5Bwyv1u33jY6xxLY3n9JHBcjhoFsgZ-OaE",
+          },
+        }
+      )
+      if (response.ok) {
+        alert("Recensione inviata!")
+        this.setState({
           comment: {
-            ...comment,
-            elementId: props.asin,
+            comment: "",
+            rate: 1,
+            elementId: this.props.asin,
           },
         })
-      }try {
-        let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/comments",
-          {
-            method: "POST",
-            body: JSON.stringify(comment),
-            headers: {
-              "Content-type": "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODcwYmIzMjc4Y2RkZjAwMTU1ZDY3OWYiLCJpYXQiOjE3NTM3OTIzMjMsImV4cCI6MTc1NTAwMTkyM30.iucPMUxGD5Bwyv1u33jY6xxLY3n9JHBcjhoFsgZ-OaE",
-            },
-          }
-        )
-        if (response.ok) {
-          alert("Recensione inviata!")
-          setComment({
-            comment: {
-              comment: "",
-              rate: 1,
-              elementId: props.asin,
-            },
-          })
-        } else {
-          throw new Error("Qualcosa è andato storto")
-        }
-      } catch (error) {
-        alert(error)
+        setComment({
+          comment: {},
+        })
+      } else {
+        throw new Error("Qualcosa è andato storto")
       }
+    } catch (error) {
+      alert(error)
     }
-    sendComment()
-  }, [props.asin])
+  }
 
-      return (
-        <div className="my-3">
-          <Form onSubmit={this.sendComment}>
-            <Form.Group className="mb-2">
-              <Form.Label>Recensione</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Inserisci qui il testo"
-                value={comment.comment}
-                onChange={(e) =>
-                  setComment({
-                    comment: {
-                      ...comment,
-                      comment: e.target.value,
-                    },
-                  })
-                }
-              />
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label>Valutazione</Form.Label>
-              <Form.Control
-                as="select"
-                value={this.state.comment.rate}
-                onChange={(e) =>
-                  setRate({
-                    comment: {
-                      ...comment,
-                      rate: e.target.value,
-                    },
-                  })
-                }
-              >
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Invia
-            </Button>
-          </Form>
-        </div>
-      )
-    }
-
-    
+  render() {
+    return (
+      <div className="my-3">
+        <Form onSubmit={this.sendComment}>
+          <Form.Group className="mb-2">
+            <Form.Label>Recensione</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Inserisci qui il testo"
+              value={this.state.comment.comment}
+              onChange={(e) =>
+                this.setState({
+                  comment: {
+                    ...this.state.comment,
+                    comment: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Group>
+          <Form.Group className="mb-2">
+            <Form.Label>Valutazione</Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.comment.rate}
+              onChange={(e) =>
+                this.setState({
+                  comment: {
+                    ...this.state.comment,
+                    rate: e.target.value,
+                  },
+                })
+              }
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Invia
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
 
 export default AddComment
--- epibooks-4-main/src/components/CommentArea.jsx
+++ epibooks-4-main/src/components/CommentArea.jsx
@@ -9,36 +9,9 @@ const CommentArea = (props) => {
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
 
-  // componentDidMount = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       'https://striveschool-api.herokuapp.com/api/comments/' +
-  //         this.props.asin,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Bearer inserisci-qui-il-tuo-token',
-  //         },
-  //       }
-  //     )
-  //     console.log(response)
-  //     if (response.ok) {
-  //       let comments = await response.json()
-  //       this.setState({ comments: comments, isLoading: false, isError: false })
-  //     } else {
-  //       console.log('error')
-  //       this.setState({ isLoading: false, isError: true })
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //     this.setState({ isLoading: false, isError: true })
-  //   }
-  // }
   useEffect(() => {
     const getComments = async () => {
-      setIsLoading({
-        isLoading: true,
-      })
+      setIsLoading(true)
       try {
         let response = await fetch(
           "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
@@ -56,8 +29,7 @@ const CommentArea = (props) => {
           setIsLoading(false)
           setIsError(false)
         } else {
-          setIsLoading(false)
-          setIsError(true)
+          throw new Error(`Error in fetching`)
         }
       } catch (error) {
         console.log(error)
